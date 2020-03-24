f2666b3b0cba2a400d7c697f5aa954a7601508c9
Ahmed Mansour <ahmed@mansour.fi>
2020-03-24 20:24:43 +0200

    Init styles variables

---
+++ frontend/src/styles/base.ts
@@ -0,0 +1,7 @@
+export const shadows = {
+  '1': '1px 1px 5px 0 rgba(0,0,0,.02), 1px 1px 15px 0 rgba(0,0,0,.03)',
+  '2': '0 3px 6px rgba(0, 0, 0, 0.15), 0 2px 4px rgba(0, 0, 0, 0.12)',
+  '3': '0 10px 20px rgba(0, 0, 0, 0.15), 0 3px 6px rgba(0, 0, 0, 0.1)',
+  '4': '0 15px 25px rgba(0, 0, 0, 0.15), 0 5px 10px rgba(0, 0, 0, 0.05)',
+  '5': '0 20px 40px rgba(0, 0, 0, 0.2)'
+};
