41ae6b277f1ee72c540b931792d650d36ab6b986
Ahmed Mansour <mail+git@mansour.fi>
2020-05-28 18:59:41 +0300

    Init backend CI pipeline using Github Actions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // @TODO change this to an env variable.
+  await app.listen(80);
 }
 bootstrap();
