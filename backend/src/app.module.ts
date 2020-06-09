d5fca1d5dcb230c88e1d09352cfe58dbf2100206
Anders Nylund <anders@andersnylund.com>
2020-06-09 17:32:26 +0300

    Add modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ormOptions } from './db';
+import { ThemeModule } from './theme/theme.module';
+import { ExperiencesModule } from './experiences/experiences.module';
 
 @Module({
   controllers: [AppController],
-  imports: [TypeOrmModule.forRoot(ormOptions)],
+  imports: [TypeOrmModule.forRoot(ormOptions), ThemeModule, ExperiencesModule],
   providers: [AppService],
 })
 export class AppModule {}
