978c82407d9c6b4a29377ac7e412ce6ddd589b54
Ahmed Mansour <mail+git@mansour.fi>
2020-06-16 18:14:27 +0300

    Init styles

---
+++ frontend/src/styles/typography.ts
@@ -0,0 +1,17 @@
+export const fontWeight = {
+  bold: 700,
+  medium: 600,
+  regular: 400,
+};
+
+export const lineHeight = {
+  regular: 1.5,
+  small: 1.2,
+};
+
+export const fontSize = {
+  small: '1rem',
+  normal: '1.125rem',
+  medium: '1.25rem',
+  large: '1.5rem',
+};
