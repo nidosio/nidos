afd77af5481fd5ca667aa7cff3816d63c4b2de81
Ahmed Mansour <mail+git@mansour.fi>
2020-06-16 18:14:27 +0300

    Init styles

---
+++ frontend/src/styles/dimensions.ts
@@ -0,0 +1,20 @@
+const baseSpacing = 1.6; // rems
+
+export const spacing = {
+  '1': `${baseSpacing * 0.25}rem`,
+  '2': `${baseSpacing * 0.5}rem`,
+  '3': `${baseSpacing * 0.75}rem`,
+  '4': `${baseSpacing * 1}rem`,
+  '5': `${baseSpacing * 1.5}rem`,
+  '6': `${baseSpacing * 2}rem`,
+  '7': `${baseSpacing * 3}rem`,
+  '8': `${baseSpacing * 4}rem`,
+  '9': `${baseSpacing * 6}rem`,
+  '10': `${baseSpacing * 8}rem`,
+  '11': `${baseSpacing * 12}rem`,
+  '12': `${baseSpacing * 16}rem`,
+  '13': `${baseSpacing * 24}rem`,
+  '14': `${baseSpacing * 32}rem`,
+  '15': `${baseSpacing * 40}rem`,
+  '16': `${baseSpacing * 48}rem`,
+};
